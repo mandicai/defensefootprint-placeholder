9d83b50fa40cbc886340f903b8e6bf6f1a866e9a
Mandi Cai <mandi@biobright.org>
2018-07-08 22:04:11 -0400

    Background image, form, styling

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,44 @@
 $(document).ready(function() {
 
+	/* ======== Text fade ====== */
 	$('#fade-in').hide(0).fadeIn(2000)
+
+	/* ======== Contact form ====== */
+	if ($("#FORMCARRY-CONTACT-US").length) {
+    // if you try to run .validate without checking if the element exists, it breaks
+    // the navbar scrolling effects further down in this script.
+    // Calling validate with rules/options actives the validation error messages.
+    // submitHandler runs if the form is valid and the user clicks submit.
+    // Log into the BioBright Formcarry account to change the URL
+
+      $("#FORMCARRY-CONTACT-US").validate({
+        rules: {
+          EMAIL: {
+            required: true,
+            email: true
+          }
+        },
+        submitHandler: function(form) {
+          $.ajax({
+            url: "https://formcarry.com/s/HvL9NCYOm1",
+            method: "POST",
+            data: {
+              name: $(form).find("input[name='NAME']").val(),
+              email: $(form).find("input[name='EMAIL']").val(),
+              message: $(form).find("textarea[name='MESSAGE']").val(),
+            },
+            dataType: "json",
+            success: function(form) {
+              $('#formcarry-submit-success').append('<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Thanks for your interest!</div>');
+            },
+            error: function() {
+              $('#formcarry-submit-error').append('<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Sorry, there was an error. Please email us directly at <a href="mailto:info@biobright.com">info@biobright.com</a></div>');
+            }
+        })
+        form.reset()
+      }
+    })
+  }
 
 	/* ======== Javascript Document ====== */
 	$('#subscribeform').submit(function(){
