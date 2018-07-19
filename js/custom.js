f4dd6263bc88c947481e67f08acd73df6c71fae4
Mandi Cai <mandi@biobright.org>
2018-07-19 11:24:41 -0400

    Add in google analytics

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+  /* ======== Google Analytics ====== */
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-122616818-1');
+
 	/* ======= Scroll to Contact ======= */
   $("#send-message, #send-message-two").click(function() {
     $('html,body').animate({
@@ -49,33 +56,25 @@ $(document).ready(function() {
 
 	/* ======== Javascript Document ====== */
 	$('#subscribeform').submit(function(){
-		var action = $(this).attr('action');
+		var action = $(this).attr('action')
 			$("#mesaj").slideUp(750,function() {
-			$('#mesaj').hide();
+			$('#mesaj').hide();
 		$('#subsubmit')
 				.after('')
-				.attr('disabled','disabled');
+				.attr('disabled','disabled')
 		$.post(action, {
 				email: $('#subemail').val()
 			},
 				function(data){
-					document.getElementById('mesaj').innerHTML = data;
-					$('#mesaj').slideDown('slow');
-					$('#subscribeform img.subscribe-loader').fadeOut('slow',function(){$(this).remove()});
-					$('#subsubmit').removeAttr('disabled');
-					if(data.match('success') != null) $('#subscribeform').slideUp('slow');
+					document.getElementById('mesaj').innerHTML = data
+					$('#mesaj').slideDown('slow')
+					$('#subscribeform img.subscribe-loader').fadeOut('slow',function(){$(this).remove()})
+					$('#subsubmit').removeAttr('disabled')
+					if(data.match('success') != null) $('#subscribeform').slideUp('slow')
 				}
 			);
 			});
-			return false;
+			return false
 		});
 
-	/* ======== Google Analytics ====== */
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-38869757-3', 'auto');
-  ga('send', 'pageview');
 })
